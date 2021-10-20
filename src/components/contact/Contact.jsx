0f64b4a9bc9fb3b8ff02cc9dc7b204ed4f8cf960
ayoub-bani <ayoub.bani101@gmail.com>
2021-10-21 00:34:48 +0100

    emailjs functions && making my form functional

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,26 @@
-import "./contact.css"
-import Phone from "../../img/phone.png"
-import Email from "../../img/email.png"
-import Address from "../../img/address.png"
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useRef, useState } from "react";
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const formRef = useRef()
+    const [done, setDone] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_oref5uf', 'template_ph6vere', formRef.current, 'user_6aVk7ZOs1Q6QkVcZitNDL')
+            .then((result) => {
+                console.log(result.text);
+                setDone(true)
+
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
         <div className="c">
             <div className="c-bg"></div>
@@ -30,12 +47,13 @@ const Contact = () => {
                         <b>What’s your story?</b> Get in touch. Always available for
                         freelancing if the right project comes along. me.
                     </p>
-                    <form>
+                    <form ref={formRef} onSubmit={handleSubmit}>
                         <input type="text" placeholder="Name" name="user_name" />
                         <input type="text" placeholder="Subject" name="user_subject" />
                         <input type="text" placeholder="Email" name="user_email" />
                         <textarea rows="5" placeholder="message" name="message"></textarea>
                         <button>Submit</button>
+                        {done && "Your Message has been sent succesfully"}
                     </form>
                 </div>
             </div>
