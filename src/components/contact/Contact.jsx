e920d8963fb9a916121eb06b417ae4417c70ed9c
ayoub-bani <ayoub.bani101@gmail.com>
2021-10-21 23:52:24 +0100

    toggle component & dark mode

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,16 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import emailjs from 'emailjs-com';
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
     const formRef = useRef()
     const [done, setDone] = useState(false)
+    // dark mode
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -48,10 +52,10 @@ const Contact = () => {
                         freelancing if the right project comes along. me.
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Name" name="user_name" />
-                        <input type="text" placeholder="Subject" name="user_subject" />
-                        <input type="text" placeholder="Email" name="user_email" />
-                        <textarea rows="5" placeholder="message" name="message"></textarea>
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
+                        <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="message" name="message"></textarea>
                         <button>Submit</button>
                         {done && "Your Message has been sent succesfully"}
                     </form>
